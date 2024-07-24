cd2da201dca5dc5cb80cd770d2ac2309943b51f8
임종완 <pjlimstar@keystronehands.com>
2024-07-24 12:41:30 +0900

    change test case class namming

--- src/test/java/com/limstar/musinsa/MusinsaControllerTest.java
+++ src/test/java/com/limstar/musinsa/MusinsaControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("api test case")
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ProductApiTest {
+public class MusinsaControllerTest {
     @Autowired
     protected MockMvc mockMvc;
 
