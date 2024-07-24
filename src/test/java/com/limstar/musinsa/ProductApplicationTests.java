b62ccfb99bd12cc64ff13cdc7753774bc7c5720e
임종완 <pjlimstar@keystronehands.com>
2024-07-24 09:52:05 +0900

    update testcase

--- src/test/java/com/limstar/musinsa/ProductApplicationTests.java
+++ src/test/java/com/limstar/musinsa/ProductApplicationTests.java
@@ -1,14 +1,22 @@
 package com.limstar.musinsa;
 
 import com.limstar.musinsa.entity.Product;
+import com.limstar.musinsa.repository.ProductRepository;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.fail;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ProductApplicationTests {
+	@Autowired
+	ProductRepository productRepository;
 
 	@BeforeAll
 	static void init() {
@@ -17,7 +25,7 @@ class ProductApplicationTests {
 
 	@BeforeEach
 	void setUp() {
-		System.out.println("start etUp");
+		System.out.println("start setUp");
 
 	}
 
@@ -35,18 +43,59 @@ class ProductApplicationTests {
 	}
 	@Test
 	@DisplayName("생성")
-	public void create() {
+	public void saveTest() {
+		Product product = Product.builder()
+				.brand("J")
+				.category("양말")
+				.price(5000)
+				.build();
+
+		Product savedProduct = productRepository.save(product);
+		Assertions.assertEquals("J", savedProduct.getBrand());
+		Assertions.assertEquals("양말", savedProduct.getCategory());
+		Assertions.assertEquals(5000, savedProduct.getPrice());
 	}
 
 	@Test
 	@DisplayName("가져오기")
 	public void get() {
+		Product product = Product.builder()
+				.brand("K")
+				.category("양말")
+				.price(5000)
+				.build();
 
+		Product savedProduct = productRepository.save(product);
+
+		Product foundProduct = productRepository.findProductById(savedProduct.getId());
+
+		Assertions.assertEquals("K", foundProduct.getBrand());
+		Assertions.assertEquals("양말", foundProduct.getCategory());
+		Assertions.assertEquals(5000, foundProduct.getPrice());
 	}
+
 	@Test
 	@DisplayName("업데이트")
 	public void update() {
+		Product product = Product.builder()
+				.brand("L")
+				.category("양말")
+				.price(5000)
+				.build();
 
+		productRepository.save(product);
+
+		Product modifiedProduct = Product.builder().brand("M").category("장갑").price(6300).build();
+
+		Product saveModifiedProduct = productRepository.save(modifiedProduct);
+
+		Assertions.assertNotEquals(product.getCategory(), saveModifiedProduct.getCategory());
+		Assertions.assertNotEquals(product.getBrand(), saveModifiedProduct.getBrand());
+		Assertions.assertNotEquals(product.getPrice(), saveModifiedProduct.getPrice());
+
+		Assertions.assertEquals("장갑", saveModifiedProduct.getCategory());
+		Assertions.assertEquals("M", saveModifiedProduct.getBrand());
+		Assertions.assertEquals(6300, saveModifiedProduct.getPrice());
 	}
 
 	@Test
